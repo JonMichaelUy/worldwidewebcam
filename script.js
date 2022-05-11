992f84b372e839d9663613303e1e188eea875df9
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-05-11 10:32:44 -0400

    add dropdown to be able to select category

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//ELEMENT REFERENCES
+const $title = $('#title')
+const $city = $('#city')
+const $region = $('#region')
+const $country = $('#country')
+const $iframe = $('iframe')
+
+
+
 //VARIABLES
 
 const randomURL = 'https://api.windy.com/api/webcams/v2/list/orderby=random/limit=50?show=webcams:image,location,player;categories&key=xMMGgPgLclxZ4KEX9vDrtMTweoIiHQqm'
@@ -12,25 +21,29 @@ $("select#category").change(function() {
   // console.log(category);
 
 
-  const categoryURL = `https://api.windy.com/api/webcams/v2/list/limit=50/category=${category}?key=xMMGgPgLclxZ4KEX9vDrtMTweoIiHQqm`
+  const categoryURL = `https://api.windy.com/api/webcams/v2/list/orderby=random/category=${category}/limit=50?show=webcams:location,player&key=xMMGgPgLclxZ4KEX9vDrtMTweoIiHQqm`
   // console.log(categoryURL)
 
-  
+  $.ajax(categoryURL).then(function(data){
+    
+    console.log(data)
+
+    let url= data['result']['webcams'][0]['player']['day']['embed']
+
+    $title.text('Title: ' + data['result']['webcams'][0]['title']);
+    $city.text('City: ' + data['result']['webcams'][0]['location']['city']);
+    $region.text('Region: ' + data['result']['webcams'][0]['location']['region']);
+    $country.text('Country: ' + data['result']['webcams'][0]['location']['country']);
+
+    $iframe.attr('src',url)
+
+    // let newDropdown = $('<select />')
+    // newDropdown.appendTo('form')
+    // $('<option />', {text: $(data['result']['webcams'])}).appendTo(newDropdown)
+  })
 
 
 })
-
-
-
-//ELEMENT REFERENCES
-const $title = $('#title')
-const $city = $('#city')
-const $region = $('#region')
-const $country = $('#country')
-const $iframe = $('iframe')
-
-
-//EVENT LISTENERS
 
 
 
